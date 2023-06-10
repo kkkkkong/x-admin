ff6422105c0adfdafe1a05043bb80c11dafa9cee
kong <376634352@qq.com>
2023-06-10 17:28:19 +0800

    添加基础配置

--- src/test/java/com/admin/XAdminApplicationTests.java
+++ src/test/java/com/admin/XAdminApplicationTests.java
@@ -1,13 +1,23 @@
 package com.admin;
 
+import com.admin.sys.entity.User;
+import com.admin.sys.mapper.UserMapper;
+import com.admin.sys.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @SpringBootTest
 class XAdminApplicationTests {
+    @Resource
+    private UserMapper userMapper;
 
     @Test
-    void contextLoads() {
+    void testUserMapper() {
+        List<User> users = userMapper.selectList(null);
+        users.forEach(System.out::println);
     }
 
 }
